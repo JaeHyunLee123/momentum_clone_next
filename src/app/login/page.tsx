e8a548c9cfdffc4f2aff2555aa4165b12128ae8c
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-08-03 15:58:24 +0900

    feat: UI of login and sign up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { sign } from "crypto";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
 
 const login = "login";
 const signup = "signup";
@@ -11,13 +13,41 @@ interface LoginPageProps {
 export default function Login({ loginOrSignup = login }: LoginPageProps) {
   return (
     <div>
-      <Tabs defaultValue="loginOrSignUp">
+      <Tabs defaultValue={loginOrSignup} className="w-[500px]">
         <TabsList>
           <TabsTrigger value={login}>Log In</TabsTrigger>
           <TabsTrigger value={signup}>Sign Up</TabsTrigger>
         </TabsList>
-        <TabsContent value={login}>Login</TabsContent>
-        <TabsContent value={signup}>Singup</TabsContent>
+        <TabsContent value={login}>
+          <Card>
+            <CardHeader>Log In</CardHeader>
+            <CardContent>
+              <form>
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" />
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" />
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value={signup}>
+          <Card>
+            <CardHeader>Sign Up</CardHeader>
+            <CardContent>
+              <form>
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" />
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" />
+                <Label htmlFor="passwordConfirmation">
+                  Password Confirmation
+                </Label>
+                <Input id="passwordConfirmation" />
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
       </Tabs>
     </div>
   );
